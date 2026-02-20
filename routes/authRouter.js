b2bb1fb0f131dca0d05f9c249c96568d5a76a888
variyarudrax <rudrax.variya@bacancy.com>
2026-02-20 15:04:52 +0530

    Add rate limiting to authentication routes
    - Implemented rate limiting for the registration and login endpoints to prevent abuse and enhance security.
    - Introduced express-rate-limit middleware to restrict the number of requests within a specified time frame.

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,8 +6,16 @@ import {
   validateLoginInput,
 } from "../middleware/validateMiddleware.js";
 
-router.post("/register", validateRegisterInput, register);
-router.post("/login", validateLoginInput, login);
+import rateLimiter from "express-rate-limit";
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 15,
+  message: "Too many requests, please try again later.",
+});
+
+router.post("/register", apiLimiter, validateRegisterInput, register);
+router.post("/login", apiLimiter, validateLoginInput, login);
 router.get("/logout", logout);
 
 export default router;
