4c59d540d2fa9d5913d4d84df6f3876ac2ad6d7b
variyarudrax <rudrax.variya@bacancy.com>
2026-02-25 16:44:07 +0530

    Implement multer middleware for file uploads with dynamic directory creation
    - Enhanced multer middleware to create the uploads directory dynamically if it doesn't exist.
    - Updated the destination path for file uploads to use a calculated directory path based on the current file location.

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,8 +1,17 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.join(__dirname, "..", "public", "uploads");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/uploads");
+    fs.mkdirSync(uploadDir, { recursive: true });
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     const fileName = file.originalname;
